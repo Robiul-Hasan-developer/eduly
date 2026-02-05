4564aaf724f146b7379b3efd182d84c444f5c49c
Robiul Hasan <robiulhasan9559@gmail.com>
2026-02-05 13:27:44 +0600

    (section) Category section done with radial progress

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -948,6 +948,42 @@
       });
     // ========================= Custom Accordion End ===================
 
+     // ========================== Add Attribute For Bg Image Js Start ====================
+    $(".background-img").css("background", function () {
+      var bg = "url(" + $(this).data("background-image") + ")";
+      return bg;
+    });
+    // ========================== Add Attribute For Bg Image Js End =====================
+
+     // ========================= Animated Radial Progress Js Start ===================
+    function animateProgress() {
+      $("svg.radial-progress").each(function () {
+        // Check if the element is within the viewport
+        const elementTop = $(this).offset().top;
+        const elementBottom = elementTop + $(this).outerHeight();
+        const viewportTop = $(window).scrollTop();
+        const viewportBottom = viewportTop + $(window).height();
+
+        if (elementBottom > viewportTop && elementTop < viewportBottom) {
+          const percent = $(this).data("percentage");
+          const radius = $(this).find("circle.complete").attr("r");
+          const circumference = 2 * Math.PI * radius;
+          const strokeDashOffset =
+            circumference - (percent / 100) * circumference;
+
+          // Animate the circle
+          $(this)
+            .find("circle.complete")
+            .css("stroke-dashoffset", strokeDashOffset);
+        }
+      });
+    }
+
+    // Trigger animation on scroll and page load
+    $(window).on("scroll", animateProgress);
+    animateProgress(); // Run on page load
+    // ========================= Animated Radial Progress Js End ===================
+
   });
   // ==========================================
   //      End Document Ready function
